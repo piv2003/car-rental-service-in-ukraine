03eaa84b526ec33761ea94b21850562ad720e40f
Ihor Poliakov <piv2003@gmail.com>
2023-11-13 23:20:31 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,24 @@
 import { useState } from 'react';
 import { ButtonClass } from './Button.styled';
 import PropTypes from 'prop-types';
+
+export const Button = ({ onClick = null }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    await onClick();
+    setIsLoading(false);
+  };
+
+  return (
+    <ButtonClass
+      type="button"
+      className={Button}
+      onClick={handleClick}
+      disabled={isLoading}
+    >
+      {isLoading ? 'Loading...' : 'Load more'}
+    </ButtonClass>
+  );
+};
