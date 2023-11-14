f90dfb7c9c660a3840ba3b6b5f9ec36feeccdce6
Ihor Poliakov <piv2003@gmail.com>
2023-11-14 21:02:52 +0200

    create App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Loader from './Loader/Loader';
+import { Header } from '../components/Header/Header';
+import { selectLoading } from '../redux/selectors';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
+const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+
 export const App = () => {
+  const isLoading = useSelector(selectLoading);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Route>
+      </Routes>
+      {isLoading && <Loader />}
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import App from './components/App';
+import { App } from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
