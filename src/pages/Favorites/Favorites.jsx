995bc00c826cc7ab6811607a01892e91e9e3220f
Ihor Poliakov <piv2003@gmail.com>
2023-11-14 20:13:56 +0200

    Update Favorites.jsx

---
+++ src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectFavoritesCars } from '../../redux/selectors';
+import { deleteAllFavoriteCars } from '../../redux/favoriteSlice';
+import { DeleteBtn, LinkCatalog } from './Favorites.styled';
+import { CarsList } from '../../components/CarsList/CarsList';
+import { NotFound } from '../../components/NotFound/NotFound';
+import { ScrollToTop } from '../../components/ScrollToTop/ScrollToTop';
+
+
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Favorites;
