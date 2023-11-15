97e97524ac83def85d98dac476dba349c448aa3a
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 21:38:49 +0200

    Update SearchForm.styled.js

---
+++ src/components/SearchForm/SearchForm.styled.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  margin: 0;
+  color: #8a8a89;
+  font-family: Manrope;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin: 0;
+  background-color: #f5f5f5;
+  gap: 20px;
+`;
+
+export const SubForm = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  gap: 20px;
+`;
+
+export const EnterText = styled.div`
+  display: flex;
+  width: 320px;
+  height: 40px;
+  border: none;
+  align-items: flex-start;
+  margin-top: 20px;
+
+  margin: 0;
+`;
