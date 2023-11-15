21e34d25d48579a472bad028b18bf7fad368a793
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 17:17:45 +0200

    Update ScrollToTop.jsx

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react';
+import { IconContext } from 'react-icons';
+import { DiRuby } from 'react-icons/di';
+import { ScrollButton } from './ScrollToTop.styled';
+
+export const ScrollToTop = () => {};
