20fee80534b8dbd2ee5bb34ca444568c69a8a139
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 21:24:59 +0200

    Update Favorites.jsx

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -6,11 +6,32 @@ import { CarsList } from '../../components/CarsList/CarsList';
 import { NotFound } from '../../components/NotFound/NotFound';
 import { ScrollToTop } from '../../components/ScrollToTop/ScrollToTop';
 
+const Favorites = () => {
+  const dispatch = useDispatch();
+  const favoriteCars = useSelector(selectFavoritesCars);
 
+  const handleDeleteAll = () => {
+    dispatch(deleteAllFavoriteCars());
+  };
 
   return (
     <>
-      
+      <div>
+        {favoriteCars.length > 0 ? (
+          <>
+            <CarsList cars={favoriteCars} />
+            <DeleteBtn onClick={handleDeleteAll}>
+              delite all favorites cars
+            </DeleteBtn>
+          </>
+        ) : (
+          <>
+            <NotFound />
+            <LinkCatalog to="/catalog">Add some favorites cars</LinkCatalog>
+          </>
+        )}
+      </div>
+      <ScrollToTop />
     </>
   );
 };
