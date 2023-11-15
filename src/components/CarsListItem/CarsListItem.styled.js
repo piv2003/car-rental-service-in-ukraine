ea925f6da2f518873b17a9a4065dfc02fd288037
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 21:34:15 +0200

    Update CarsListItem.styled.js

---
+++ src/components/CarsListItem/CarsListItem.styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { ReactComponent as GoHeart } from '../../images/heart.svg';
+
+export const Item = styled.li`
+  position: relative;
+  border-radius: 14px;
+  max-width: 474px;
+  min-height: 426px;
+  overflow: hidden;
+  gap: 10px;
+  background-color: #f5f5f5;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+`;
+
+export const ImageThumb = styled.div`
+  height: 268px;
+  border-radius: 14px;
+  overflow: hidden;
+`;
+
+export const Image = styled.img`
+  height: 268px;
+  object-fit: contain;
+  object-position: 50% 50%;
+`;
+
+export const Paragraf = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10px;
+  margin: 20px 0;
+`;
