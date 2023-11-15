f85f812dd9673edc7d3cd8649fcdeff3f6c2ef18
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 17:18:22 +0200

    Update ScrollToTop.jsx

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -3,4 +3,22 @@ import { IconContext } from 'react-icons';
 import { DiRuby } from 'react-icons/di';
 import { ScrollButton } from './ScrollToTop.styled';
 
-export const ScrollToTop = () => {};
+export const ScrollToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      {showTopBtn && (
+        <ScrollButton onClick={goToTop}>
+          <IconContext.Provider value={{}}>
+            <DiRuby />
+          </IconContext.Provider>
+          <span> To Top</span>
+        </ScrollButton>
+      )}
+    </>
+  );
+};
