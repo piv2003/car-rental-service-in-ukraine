e3ae0956de4eb59100ef62b70278514d802887cc
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 20:37:51 +0200

    Update SearchForm.jsx

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,11 @@
+import Select from 'react-select';
+import { useDispatch } from 'react-redux';
+import {
+  BtnSearch,
+  EnterText,
+  Form,
+  LabelMilage1,
+  LabelMilage2,
+  SubForm,
+  Title,
+} from './SearchForm.styled';
