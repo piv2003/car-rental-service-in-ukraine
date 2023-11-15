563b53171c73d0a38c3b6c7a64807ad8265a8827
Ihor Poliakov <piv2003@gmail.com>
2023-11-15 17:07:38 +0200

    Update favoriteSlice.js

--- src/redux/favoriteSlice.js
+++ src/redux/favoriteSlice.js
@@ -4,6 +4,19 @@ const initialState = {
   favoritCar: [],
 };
 
+const favoriteSlice = createSlice({
+  name: 'favorite',
+  initialState,
+  reducers: {
+    addFavoriteCar: (state, action) => {
+      state.favoritCar.push(action.payload);
+    },
+    deleteAllFavoriteCars: state => {
+      state.favoritCar = [];
+    },
+  },
+});
+
 export const favoriteReducer = favoriteSlice.reducer;
 export const { addFavoriteCar, deleteAllFavoriteCars, deleteFavoritCar } =
   favoriteSlice.actions;
