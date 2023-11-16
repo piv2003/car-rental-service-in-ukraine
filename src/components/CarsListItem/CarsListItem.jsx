b03cf94d6b4d16f92cdc47fa914cb2724eefb04a
Ihor Poliakov <piv2003@gmail.com>
2023-11-16 14:31:46 +0200

    Update CarsListItem.jsx

--- src/components/CarsListItem/CarsListItem.jsx
+++ src/components/CarsListItem/CarsListItem.jsx
@@ -15,3 +15,68 @@ import {
 import { Modal } from '../../components/Modal/Modal';
 import { selectFavoritesCars } from '../../redux/selectors';
 import { addFavoriteCar, deleteFavoritCar } from '../../redux/favoriteSlice';
+
+export const CarsListItem = ({ car }) => {
+  const [toggleModal, setToggleModal] = useState(false);
+  const dispatch = useDispatch();
+  const favoriteCars = useSelector(selectFavoritesCars);
+  const {
+    id,
+    year,
+    make,
+    model,
+    type,
+    img,
+    accessories,
+    functionalities,
+    rentalPrice,
+    rentalCompany,
+    address,
+  } = car;
+  const updateaddress = address.split(', ').slice(-2).join(' | ');
+  const isInFavorites = favoriteCars.some(i => i.id === id);
+
+  return (
+    <>
+      <Item>
+        <ImageThumb>
+          <Image src={img} alt={model} />
+        </ImageThumb>
+        <Paragraf>
+          <p>
+            {make} <Span> {model}, </Span> {year}
+          </p>
+          <p> {rentalPrice}</p>
+        </Paragraf>
+        <Descr>
+          {updateaddress} | {rentalCompany} | {make} | {id} | {type} |{' '}
+          {accessories[0]} | {functionalities[0]}
+        </Descr>
+        <Btn id={id} onClick={handleClick}>
+          learn more
+        </Btn>
+        <StyledHeart id={id} onClick={() => handleToogleFavorites(id)}>
+          <StyledHeartIcon $isInFavorites={isInFavorites} />
+        </StyledHeart>
+      </Item>
+      {toggleModal && <Modal handleClick={handleClick} car={car}></Modal>}
+    </>
+  );
+};
+
+CarsListItem.propTypes = {
+  car: PropTypes.shape({
+    id: PropTypes.number,
+    year: PropTypes.number,
+    make: PropTypes.string,
+    model: PropTypes.string,
+    type: PropTypes.string,
+    img: PropTypes.string,
+    description: PropTypes.string,
+    accessories: PropTypes.arrayOf(PropTypes.string),
+    functionalities: PropTypes.arrayOf(PropTypes.string),
+    rentalPrice: PropTypes.string,
+    rentalCompany: PropTypes.string,
+    address: PropTypes.string,
+  }),
+};
