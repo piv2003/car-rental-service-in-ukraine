f5b3d29e0d0a426d69aa23f180928cf76b9303c3
Ihor Poliakov <piv2003@gmail.com>
2023-11-17 23:31:46 +0200

    fixing errors

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -13,7 +13,7 @@ import {
   Driver,
   Functional,
   Image,
-  ImageThumb,
+  ThumbImage,
   Item,
   MilegEPrice,
   Milege,
@@ -28,6 +28,113 @@ import {
   User,
 } from './Modal.styled';
 import { numberWithSlash } from '../../helpers/numberWithSlash';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ handleClick, car }) => {
+  useEffect(() => {
+    const handlePressKey = event => {
+      if (event.code === 'Escape') {
+        handleClick();
+      }
+    };
+    document.body.classList.add('no-scroll');
+    window.addEventListener('keydown', handlePressKey);
+
+    return () => {
+      window.removeEventListener('keydown', handlePressKey);
+      document.body.classList.remove('no-scroll');
+    };
+  }, [handleClick]);
+
+  const handleOverlayClick = e => {
+    if (e.target === e.currentTarget) {
+      handleClick();
+    }
+  };
+  const {
+    id,
+    year,
+    make,
+    model,
+    type,
+    img,
+    description,
+    fuelConsumption,
+    engineSize,
+    accessories,
+    functionalities,
+    rentalPrice,
+    address,
+    rentalConditions,
+    mileage,
+  } = car;
+
+  const updateaddress = address.split(', ').slice(-2).join(' | ');
+  const rentalConditionsToArray = rentalConditions.split('\n');
+  const userconditions = rentalConditionsToArray[0];
+  const userConditionsDescr = userconditions.split(': ');
+  const driverLicense = rentalConditionsToArray[1];
+  const securyty = rentalConditionsToArray[2];
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <ModalStyled>
+        <CloseBtn className="button" type="button" onClick={handleClick}>
+          <GrClose siz={24} />
+        </CloseBtn>
+        <Item>
+          <ThumbImage>
+            <Image src={img} alt={model} />
+          </ThumbImage>
+          <Paragraf>
+            <Title>
+              {make}
+              <Span> {model}, </Span>
+              {year}
+            </Title>
+          </Paragraf>
+
+          <Descr>
+            <Country>
+              {updateaddress} | id: {id} | Year: {year} | type: {type}
+            </Country>
+            <Country>
+              fuelConsumption: {fuelConsumption} | engineSize: {engineSize}
+            </Country>
+            <Description>{description}</Description>
+            <Functional>Accessories and functionalities:</Functional>
+            <Accessories>
+              {accessories[0]} | {accessories[1]} | {accessories[2]}
+            </Accessories>
+            <Accessories>
+              {functionalities[0]} | {functionalities[1]} | {functionalities[2]}
+            </Accessories>
+            <Rental>Rental Conditions:</Rental>
+            <User>
+              <Age>
+                {userConditionsDescr[0]}:
+                <Milege> {userConditionsDescr[1]} </Milege>
+              </Age>
+              <Driver>{driverLicense}</Driver>
+            </User>
+            <MilegEPrice>
+              <Price>{securyty}</Price>
+              <Price>
+                Mileage: <Milege>{numberWithSlash(mileage)} </Milege>
+              </Price>
+              <Price>
+                Price: <PriceCar>{rentalPrice.slice(1) + '$'}</PriceCar>
+              </Price>
+            </MilegEPrice>
+          </Descr>
+          <Tel href="tel:+380730000000"> rental car</Tel>
+        </Item>
+      </ModalStyled>
+    </Overlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   car: PropTypes.shape({
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'modern-normalize/modern-normalize.css';
 // import { store, persistor } from './redux/store';
 import { store } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
