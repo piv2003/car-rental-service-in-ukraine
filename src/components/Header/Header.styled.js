ab2503618bb1545349eada8479910f65c9a56b3e
Ihor Poliakov <piv2003@gmail.com>
2023-12-02 23:48:52 +0200

    Update Header.styled.js

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   background-color: rgb(175 238 255);
   padding: 20px;
 `;
@@ -17,7 +18,6 @@ export const PageList = styled.ul`
   display: flex;
   margin: 0;
   padding: 0;
-  margin-left: 30px;
   gap: 10px;
 `;
 
@@ -26,28 +26,49 @@ export const NavigateItem = styled.li`
   justify-content: center;
 `;
 
-export const NavigateLink = styled(NavLink)`
-  display: flex;
-  justify-content: center;
-  list-style: none;
-  text-decoration: none;
-  align-items: center;
-  width: 100px;
-  min-height: 20px;
-  padding: 10px;
-  color: #3470ff;
-  background-color: transparent;
-
-  border-radius: 6px;
-  transition: all 0.3s ease-in-out;
-  &.active {
-    background-color: #3470ff;
-    color: yellow;
+const clickEffect = keyframes`
+  0% {
+    transform: scale(1);
   }
+  50% {
+    transform: scale(0.9);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+export const NavigateLink = styled(NavLink)`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+
+  color: #ee66d0;
+  width: 200px;
+  height: 40px;
+  font-weight: 700;
+  font-size: 17px;
+  text-shadow: 1px 1px #000;
+  line-height: normal;
+  margin: 20px;
+  border: 2px solid pink;
+  border-radius: 6px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.45) 0px 5px 10px, rgba(0, 0, 0, 0.12) 0px -3px 5px,
+    rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 3px 5px,
+    rgba(0, 0, 0, 0.09) 0px -3px 2px;
+  background: radial-gradient(circle, #f1c1bc, #0c08ee);
+  background-clip: padding-box;
+  transition: background-image 0.3s, color 0.3s, transform 0.3s;
+
   &:hover {
-    scale: 1.1;
-    color: red;
-    box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.2),
-      0px 1px 5px 0px rgba(0, 0, 0, 0.14), 0px 2px 5px -1px rgba(0, 0, 0, 0.12);
+    background: radial-gradient(circle, #80d2e6, #f1c1bc);
+    color: #3232e6;
+    transform: scale(1.1);
+  }
+
+  &:active {
+    animation: ${clickEffect} 1.2s;
   }
 `;
